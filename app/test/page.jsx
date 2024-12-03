8ed2b44ca87f5220d4b83e454f277a172cc3c224
SD91PL <sd91pl@gmail.com>
2024-12-03 14:48:39 +0100

    feat: complete visual presentation of the form in test.jsx

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -27,27 +27,41 @@ export default function Test() {
 							</button>
 						</div>
 					</section>
+
 					{/* FORM */}
-					<section className='flex pt-5 w-full bg-[#FFFFFF] rounded-lg border border-[#D0D5DD]'>
-						<div className='flex flex-col w-full pl-4 sm:pl-6'>
-							<div className='flex flex-col justify-end min-h-[160px]'>
-
-								<p>FORM</p>
-
-								
+					<section className='flex pt-[1.1875rem] w-full bg-[#FFFFFF] rounded-lg border border-[#D0D5DD]'>
+						<form className='flex flex-col w-full pl-4 sm:pl-6'>
+							<div className='flex flex-col gap-2 '>
+								<div className='flex flex-col text-sm gap-1'>
+									<div className='flex gap-2'>
+										<label className='text-[#344054] font-medium'>Nazwa</label>
+										<p className='text-red-500 opacity-0'>Error</p>
+									</div>
+									<input className='py-2 px-3 border border-[#D0D5DD] rounded-lg text-[#667085] text-base outline-none shadow-sm focus:shadow transition-shadow' />
+								</div>
+								<div className='flex flex-col text-sm gap-1'>
+									<div className='flex gap-2'>
+										<label className='text-[#344054] font-medium'>Link</label>
+										<p className='text-red-500 opacity-0'>Error</p>
+									</div>
+									<input className='py-2 px-3 border border-[#D0D5DD] rounded-lg text-[#667085] text-base outline-none shadow-sm focus:shadow transition-shadow' />
+								</div>
 							</div>
-							<div className='flex justify-start items-center gap-2 min-h-[80px] text-sm font-semibold'>
+
+							<div className='flex justify-start items-center gap-2 min-h-[79px] text-sm font-semibold'>
 								<button className='py-[0.625rem] px-4 border border-[#D0D5DD] rounded-lg text-[#344054] shadow-sm hover:shadow transition-shadow'>
 									Anuluj
 								</button>
-								<button className='py-[0.625rem] px-4 border border-[#D6BBFB] rounded-lg text-[#6941C6] shadow-sm hover:shadow transition-shadow'>
+								<button
+									type='submit'
+									className='py-[0.625rem] px-4 border border-[#D6BBFB] rounded-lg text-[#6941C6] shadow-sm hover:shadow transition-shadow'>
 									Dodaj
 								</button>
 							</div>
-						</div>
+						</form>
 						<div className='min-w-[80px]'>
 							<div className='flex w-full justify-center'>
-								<button className='flex justify-center items-center w-10 h-10 hover:scale-105 transition-transform'>
+								<button className='flex justify-center items-center w-10 h-9 hover:scale-105 transition-transform'>
 									<img
 										src='/icons/trash.svg'
 										alt=''
