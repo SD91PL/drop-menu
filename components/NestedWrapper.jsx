d8679e69f81f94b30f2cec3551c995a481df5f94
SD91PL <sd91pl@gmail.com>
2024-12-18 22:34:19 +0100

    feat(NestedWrapper): adjust width of container with children to improve drag-and-drop UX

--- components/NestedWrapper.jsx
+++ components/NestedWrapper.jsx
@@ -2,7 +2,7 @@ export default function NestedWrapper({ children }) {
 	return (
 		<div className='flex w-full'>
 			<div className='mr-3 sm:mr-6 w-5 sm:w-10 shrink-0 flex-none'>{/* SPACE DIV */}</div>
-			<div className='flex flex-col w-full overflow-hidden'>{children}</div>
+			<div className='flex flex-col flex-1 w-[calc(100%-4rem)]'>{children}</div>
 		</div>
 	)
 }
